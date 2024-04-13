1c54f6efc4c4e6dea1551f968a4398ac8e3eb264
Shin Sangwoo <bano112@naver.com>
2024-04-13 20:03:08 +0900

    getter setter

--- typeScript_dream/3-oop/3-3-encapsulation.ts
+++ typeScript_dream/3-oop/3-3-encapsulation.ts
@@ -41,10 +41,33 @@
     }
   }
 
-  console.log(CoffeeMaker);
-
   const maker = CoffeeMaker.makeMachine(32);
   maker.fillCoffeeBeans(32);
 
-  console.log(maker);
+  class User {
+    get fullName(): string {
+      return `${this.firstName} ${this.lastName}`;
+    }
+
+    private internalAge = 4;
+
+    get age(): number {
+      return this.internalAge;
+    }
+
+    set age(num: number) {
+      if (num < 0) {
+      }
+      this.internalAge = num;
+    }
+
+    constructor(private firstName: string, private lastName: string) {}
+  }
+
+  const user = new User("Steve", "Jobs");
+  user.age = 6;
+
+  console.log(user.fullName);
+  user.firstName = "Ellie";
+  console.log(user.fullName);
 }
