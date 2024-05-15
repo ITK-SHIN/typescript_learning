0125ef1befbe1d412aa00d4fa347cd350d22e18f
Shin Sangwoo <bano112@naver.com>
2024-05-15 15:32:40 +0900

    3.2 : Exclude, Extract, Omit NonNullable, Optional

--- typeScript_zeroC/책3장.lib.es5.d.ts분석하기/3-2.ts
+++ typeScript_zeroC/책3장.lib.es5.d.ts분석하기/3-2.ts
@@ -10,8 +10,27 @@ type Result = MyExclude<1 | "2" | 3, string>; // type Result = 1 | 3
 type MyExtract<T, U> = T extends U ? T : never;
 type Result2 = MyExtract<1 | "2" | 3, string>; // type Result2 = "2"
 
+/* //✅ Pick : 객체에서 지정한 속성만 추리기
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+ */
+
 // ✅ Omit : 특정 객체에서 지정한 속성을 제거하는 타입
 type MyOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
-type Result3 = MyOmit<{ a: "1"; b: 2; c: true }>;
+type Result3 = MyOmit<{ a: "1"; b: 2; c: true }, "a" | "c">;
 
 // ✅ NonNullable : 타입에서 null , undefined를 제거하는 타입
+// 예전버전
+type MyNonNullable_v0<T> = T extends null | undefined ? never : T;
+type Result4 = MyNonNullable<string | number | null | undefined>;
+
+// 최근 버전
+type MyNonNullable<T> = T & {};
+type Result5 = MyNonNullable<string | number | null | undefined>;
+
+//✅ Optional 타입 : 일부 속성만 옵셔널로 만드는 타입 (lib.es5.d.ts 에 있는 타입 아님)
+type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+type Result6 = Optional<{ a: "hi"; b: 123 }, "a">;
+
+const test6: Result6 = { a: "hi", b: 123 };
