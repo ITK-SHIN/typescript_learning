c92941890a826d435562444389c2634f6ce8a275
Shin Sangwoo <bano112@naver.com>
2024-05-17 15:09:58 +0900

    3.7: filter 완성

--- typeScript_zeroC/책3장.lib.es5.d.ts분석하기/3-7.ts
+++ typeScript_zeroC/책3장.lib.es5.d.ts분석하기/3-7.ts
@@ -1,12 +1,21 @@
 //3.7: filter 만들기
 
 const r1 = [1, 2, 3].myFilter((v) => v < 2);
-const r2 = [1, 2, 3].myFilter((v, i, a) => {});
-const r3 = ["1", 2, "3"].myFilter((v) => typeof v === "string");
+const r2 = [1, 2, 3].myFilter((v, i, a): v is never => false);
+const r3 = ["1", 2, "3"].myFilter((v): v is string => typeof v === "string");
 const r4 = [{ num: 1 }, { num: 2 }, { num: 3 }].myFilter(function (v) {
-  return v.num % 2;
+  return v.num % 2 === 1;
 });
 
-interface Array<T> {
+/* interface Array<T> {
   myFilter(callback: (v: T, i: number, a: T[]) => void, thisArg?: any): T[];
+}
+ */
+
+interface Array<T> {
+  myFilter<S extends T>(
+    callback: (v: T, i: number, a: T[]) => v is S,
+    thisArg?: any
+  ): S[];
+  myFilter(callback: (v: T, i: number, a: T[]) => boolean, thisArg?: any): T[];
 }
